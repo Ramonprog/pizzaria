36c76ce205caf341286db978339445da0de23887
Ramonprog <programador.ramon@gmail.com>
2024-08-28 17:59:52 -0300

    feat: configurando layoutes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import { SignIn } from "./pages/auth/sign-in";
 import { Dashboard } from "./pages/app/dashboard";
+import { AppLayout } from "./_layouts/app";
+import { AuthLayout } from "./_layouts/auth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      }
+    ]
   },
   {
-    path: "/sign-in",
-    element: <SignIn />,
-  }
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      }
+    ]
+  },
 ]);
