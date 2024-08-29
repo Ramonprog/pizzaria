e6bc51964d8231be423fcf5307c1f5bc8eb8bfdd
Ramonprog <programador.ramon@gmail.com>
2024-08-29 11:51:57 -0300

    feat: componentizando tableRow

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
+import { OrderTableRow } from "./component/order-table-row";
 
 export function Orders() {
   return (
@@ -34,36 +33,8 @@ export function Orders() {
 
             <TableBody>
               {Array.from({ length: 10 }).map((_, index) => (
-                <TableRow key={index} >
-                  <TableCell>
-                    <Button variant='outline' size='icon'>
-                      <Search className="h-3 w-3" />
-                      <span className="sr-only">Detalhes do pedido</span>
-                    </Button>
-                  </TableCell>
-                  <TableCell className="font-mono text-xs font-medium">9201wdff9a</TableCell>
-                  <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                      <span className="font-medium text-muted-foreground">Pendente</span>
-                    </div>
-                  </TableCell>
-                  <TableCell className="font-medium">Alisson Ramon Nunes</TableCell>
-                  <TableCell className="font-medium">R$ 100,00</TableCell>
-                  <TableCell>
-                    <Button variant='outline' >
-                      <ArrowRight className="h-3 w-3 mr-2" />
-                      Aprovar
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant='ghost' >
-                      <X className="h-3 w-3 mr-2" />
-                      Cancelar
-                    </Button>
-                  </TableCell>
-                </TableRow>
+
+                <OrderTableRow key={index} />
               ))}
             </TableBody>
           </Table>
